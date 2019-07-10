0c49d2657f5ce518d4a4c2fe1486405559d84c6b
Carson Schubert <carson.schubert14@gmail.com>
2019-07-09 20:08:11 -0700

    Allow for multiple prefix selection. Fix ctrl-c behavior during filtering.

--- jigsaw/models/mask/model.py
+++ jigsaw/models/mask/model.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import PIL
 import io
 import os
+import sys
 
 from colorama import init, Fore
 from object_detection.utils import dataset_util
@@ -291,7 +292,7 @@ class LabeledImageMask(LabeledImage):
         # if yes, enter a loop that supplies filter options
         # if no, skip
         if user_confirms(
-                message="Would you like to filter out any of the data?",
+                message="Would you like to filter out any of the data ({} images total)?".format(len(tags_df)),
                 default=False):
             sets = {}
             # outer loop to determine how many sets the user will create
@@ -387,8 +388,9 @@ class LabeledImageMask(LabeledImage):
                 image_ids = join_sets(sets_to_join).index.tolist()
 
             except:
-                image_ids = tags_df.index.tolist()
-                print("Sorry, there were no tags on the data to filter by. Using all images.")
+                # image_ids = tags_df.index.tolist()
+                print("Sorry, there were no tags on the data to filter by or the user killed the program. Exiting...")
+                sys.exit(1)
         else:
             image_ids = tags_df.index.tolist()
 
